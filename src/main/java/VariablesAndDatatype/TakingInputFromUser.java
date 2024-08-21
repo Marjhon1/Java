b2c227f8302ef9a20fd3d798e76c7f6980a3b8ee
Marjhon1 <mj3939399@gmail.com>
2024-08-21 15:16:27 +0000

    first commit

---
+++ src/main/java/VariablesAndDatatype/TakingInputFromUser.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package VariablesAndDatatype;
+
+/**
+ *
+ * @author DELL
+ * 
+ */
+
+import java.untill.Scanner;
+
+public class TakingInputFromUser {
+    
+    public static void main(String [] args){
+        
+        Scanner Kami = new Scanner(System.in);
+        System.out.println();
+        
+        
+        System.out.printIn("Hello");
+    }
+    
+}
